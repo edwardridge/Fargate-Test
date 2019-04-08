326e203cee0befab46382a14f5fc0f0462a313b8
edwardridge <edwardridge@gmail.com>
2019-04-08 19:15:39 +0100

    Add project files.

---
+++ Fargate Test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fargate_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
